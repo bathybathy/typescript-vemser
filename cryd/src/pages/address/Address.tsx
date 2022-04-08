ddeba47bf023fdfbbf2a5f2adafb64309ca5eaeb
Nathalia Duarte <duartenatha@gmail.com>
2022-04-08 15:50:37 -0300

    crud funcionando no address, começo do crud no users

--- cryd/src/pages/address/Address.tsx
+++ cryd/src/pages/address/Address.tsx
@@ -28,17 +28,16 @@ import { DivForm } from "../login/Login.styles";
 export const Address: React.FC<{}> = () => {
   const [lista, setLista] = useState<any>([]);
   const [atualizar, setAtualizar] = useState<boolean>(false);
-  const formikRef:any = useRef();
-  const [idUpdate, setIdUpdate] = useState<number>()
+  const [idUpdate, setIdUpdate] = useState<number>();
 
   useEffect(() => {
     listAddress();
   }, []);
 
   // refreshes page
-  const refresh = () =>{
+  const refresh = () => {
     window.location.reload();
-}
+  };
 
   // deletes address from list
   const deleteAddress = async (id: number) => {
@@ -51,10 +50,10 @@ export const Address: React.FC<{}> = () => {
   };
 
   //gets one address from api to set it up on inputs
-  const getAddressById = async (id:number) =>{
+  const getAddressById = async (id: number) => {
     try {
-      const { data } = await api.get(`endereco/${id}`)
-      console.log(data)
+      const { data } = await api.get(`endereco/${id}`);
+      console.log(data);
       formikProps.setFieldValue("logradouro", data.logradouro);
       formikProps.setFieldValue("localidade", data.cidade);
       formikProps.setFieldValue("uf", data.estado);
@@ -62,22 +61,21 @@ export const Address: React.FC<{}> = () => {
       formikProps.setFieldValue("numero", data.numero);
       formikProps.setFieldValue("cep", data.cep);
       formikProps.setFieldValue("pais", data.pais);
-
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   // setups the update process
-  const setupUpdateAddress = (id:number) =>{
+  const setupUpdateAddress = (id: number) => {
     getAddressById(id);
-    setAtualizar(true)
+    setAtualizar(true);
     setIdUpdate(id);
-
-  }
+    console.log(id);
+  };
   // sends the updated address to api
   const updateAddress = async () => {
-    const updatedAddress ={
+    const updatedAddress = {
       cep: formikProps.values.cep,
       cidade: formikProps.values.localidade,
       complemento: formikProps.values.complemento,
@@ -86,12 +84,15 @@ export const Address: React.FC<{}> = () => {
       pais: formikProps.values.pais,
       tipo: formikProps.values.tipo,
       numero: parseInt(formikProps.values.numero),
-    }
+      idEndereco: idUpdate,
+    };
     try {
       const { data } = await api.put(`/endereco/${idUpdate}`, updatedAddress);
-      alert("cadastro editado com sucesso")
+      alert("cadastro editado com sucesso");
+      formikProps.resetForm();
+      setAtualizar(false);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
 
@@ -106,7 +107,7 @@ export const Address: React.FC<{}> = () => {
   };
 
   // gets address from cep api
-  const getAddress = async (values:string) => {
+  const getAddress = async (values: string) => {
     try {
       const { data } = await axios.get(
         `https://viacep.com.br/ws/${values}/json/`
@@ -136,7 +137,7 @@ export const Address: React.FC<{}> = () => {
     try {
       const { data } = await api.put("/endereco/650", newAddress);
       console.log(data);
-      alert("cadastro realizado com sucesso")
+      alert("cadastro realizado com sucesso");
     } catch (error) {
       console.log(error);
     }
@@ -144,40 +145,45 @@ export const Address: React.FC<{}> = () => {
 
   const formikProps = useFormik({
     initialValues: {
-    cep: "",
-    logradouro: "",
-    bairro: "",
-    localidade: "",
-    uf: "",
-    pais: "",
-    tipo: "RESIDENCIAL",
-    complemento: "",
-    numero: "",
+      cep: "",
+      logradouro: "",
+      bairro: "",
+      localidade: "",
+      uf: "",
+      pais: "",
+      tipo: "RESIDENCIAL",
+      complemento: "",
+      numero: "",
     },
-    onSubmit: async (values:any, actions:any) => {
-      console.log(atualizar)
-      if(!atualizar){
-      await postAddress(values);}
-      if(atualizar){
-        await updateAddress()
+    onSubmit: async (values: any, actions: any) => {
+      console.log(atualizar);
+      if (!atualizar) {
+        await postAddress(values);
+      }
+      if (atualizar) {
+        await updateAddress();
       }
       actions.setSubmitting(false);
     },
-  })
+  });
 
   return (
     <ContainerAddressPage>
-    <ContainerUsers>
+      <ContainerUsers>
         <DivTitle>
           <TitleUsers>Endereços</TitleUsers>
         </DivTitle>
         <ContainerForm>
-       
-         
-            <form onSubmit={formikProps.handleSubmit}>
-              <DivForm>
+          <form onSubmit={formikProps.handleSubmit}>
+            <DivForm>
               <label htmlFor="cep">CEP:</label>
-              <input id="cep" name="cep" placeholder="Digite seu cep" value={formikProps.values.cep} onChange={formikProps.handleChange}/>
+              <input
+                id="cep"
+                name="cep"
+                placeholder="Digite seu cep"
+                value={formikProps.values.cep}
+                onChange={formikProps.handleChange}
+              />
 
               <button
                 type="button"
@@ -185,126 +191,141 @@ export const Address: React.FC<{}> = () => {
               >
                 Buscar
               </button>
-              </DivForm>
+            </DivForm>
 
-              <DivForm>
+            <DivForm>
               <label htmlFor="logradouro">Logradouro:</label>
               <input
                 id="logradouro"
                 name="logradouro"
                 placeholder="Digite seu logradouro"
-                value={formikProps.values.logradouro} onChange={formikProps.handleChange}
+                value={formikProps.values.logradouro}
+                onChange={formikProps.handleChange}
               />
-              </DivForm>
+            </DivForm>
 
-              <DivForm>
+            <DivForm>
               <label htmlFor="numero">Número:</label>
               <input
                 id="numero"
                 name="numero"
                 placeholder="Digite o número da sua residência"
-                value={formikProps.values.numero} onChange={formikProps.handleChange}
+                value={formikProps.values.numero}
+                onChange={formikProps.handleChange}
               />
-              </DivForm>
+            </DivForm>
 
-              <DivForm>
+            <DivForm>
               <label htmlFor="complemento">Complemento:</label>
               <input
                 id="complemento"
                 name="complemento"
                 placeholder="Digite o seu complemento"
-                value={formikProps.values.complemento} onChange={formikProps.handleChange}
+                value={formikProps.values.complemento}
+                onChange={formikProps.handleChange}
               />
-              </DivForm>
+            </DivForm>
 
-              <DivForm>
+            <DivForm>
               <label htmlFor="bairro">Bairro:</label>
               <input
                 id="bairro"
                 name="bairro"
                 placeholder="Digite seu bairro"
-                value={formikProps.values.bairro} onChange={formikProps.handleChange}
+                value={formikProps.values.bairro}
+                onChange={formikProps.handleChange}
               />
-              </DivForm>
+            </DivForm>
 
-              <DivForm>
+            <DivForm>
               <label htmlFor="localidade">Cidade:</label>
               <input
                 id="localidade"
                 name="localidade"
                 placeholder="Digite sua localidade"
-                value={formikProps.values.localidade} onChange={formikProps.handleChange}
+                value={formikProps.values.localidade}
+                onChange={formikProps.handleChange}
               />
-              </DivForm>
+            </DivForm>
 
-              <DivForm>
+            <DivForm>
               <label htmlFor="uf">Estado:</label>
               <input
                 id="uf"
                 name="uf"
                 placeholder="Digite a sigla do seu estado"
-                value={formikProps.values.uf} onChange={formikProps.handleChange}
+                value={formikProps.values.uf}
+                onChange={formikProps.handleChange}
               />
-              </DivForm>
+            </DivForm>
 
-              <DivForm>
+            <DivForm>
               <label htmlFor="pais">País:</label>
-              <input id="pais" name="pais" placeholder="Digite o seu país" 
-              value={formikProps.values.pais} onChange={formikProps.handleChange}/>
-              </DivForm>
+              <input
+                id="pais"
+                name="pais"
+                placeholder="Digite o seu país"
+                value={formikProps.values.pais}
+                onChange={formikProps.handleChange}
+              />
+            </DivForm>
 
-              <DivForm>
+            <DivForm>
               <label htmlFor="tipo">Tipo de contato:</label>
-              <select name="tipo" value={formikProps.values.tipo} onChange={formikProps.handleChange}>
+              <select
+                name="tipo"
+                value={formikProps.values.tipo}
+                onChange={formikProps.handleChange}
+              >
                 <option value="RESIDENCIAL">Residencial</option>
                 <option value="COMERCIAL">Comercial</option>
               </select>
-              </DivForm>
+            </DivForm>
 
-              <button type="submit">Submit</button>
-            </form>
-      </ContainerForm>
-      <ContainerUsers>
-        <DivTitle>
-          <TitleUsers>Lista de endereços</TitleUsers>
-        </DivTitle>
-        <ContainerList>
-          <Table>
-            <thead>
-              <Tr>
-                <Th>Rua:</Th>
-                <Th>Número:</Th>
-                <Th>Complemento:</Th>
-                <Th>Cidade:</Th>
-                <Th>Estado:</Th>
-                <Th>País:</Th>
-                <Th>Opções:</Th>
-              </Tr>
-            </thead>
-            <tbody>
-              {lista.map((e: any) => (
-                <Tr key={e.idEndereco}>
-                  <TdNome>{e.logradouro}</TdNome>
-                  <Td>{e.numero}</Td>
-                  <Td>{e.complemento}</Td>
-                  <Td>{e.cidade}</Td>
-                  <Td>{e.estado}</Td>
-                  <Td>{e.pais}</Td>
-                  <Td>
-                    <BsPencilStyled
-                      onClick={() => setupUpdateAddress(e.idEndereco)}
-                    />
-                    <AiOutlineDeleteStyled
-                      onClick={() => deleteAddress(e.idEndereco)}
-                    />
-                  </Td>
+            <button type="submit">Submit</button>
+          </form>
+        </ContainerForm>
+        <ContainerUsers>
+          <DivTitle>
+            <TitleUsers>Lista de endereços</TitleUsers>
+          </DivTitle>
+          <ContainerList>
+            <Table>
+              <thead>
+                <Tr>
+                  <Th>Rua:</Th>
+                  <Th>Número:</Th>
+                  <Th>Complemento:</Th>
+                  <Th>Cidade:</Th>
+                  <Th>Estado:</Th>
+                  <Th>País:</Th>
+                  <Th>Opções:</Th>
                 </Tr>
-              ))}
-            </tbody>
-          </Table>
-        </ContainerList>
+              </thead>
+              <tbody>
+                {lista.map((e: any) => (
+                  <Tr key={e.idEndereco}>
+                    <TdNome>{e.logradouro}</TdNome>
+                    <Td>{e.numero}</Td>
+                    <Td>{e.complemento}</Td>
+                    <Td>{e.cidade}</Td>
+                    <Td>{e.estado}</Td>
+                    <Td>{e.pais}</Td>
+                    <Td>
+                      <BsPencilStyled
+                        onClick={() => setupUpdateAddress(e.idEndereco)}
+                      />
+                      <AiOutlineDeleteStyled
+                        onClick={() => deleteAddress(e.idEndereco)}
+                      />
+                    </Td>
+                  </Tr>
+                ))}
+              </tbody>
+            </Table>
+          </ContainerList>
+        </ContainerUsers>
       </ContainerUsers>
-    </ContainerUsers>
     </ContainerAddressPage>
   );
 };
--- cryd/src/pages/users/Users.tsx
+++ cryd/src/pages/users/Users.tsx
@@ -1,13 +1,20 @@
 import { useEffect, useState, useContext } from "react"
-
+import { useFormik } from "formik"
 import api from "../../api"
 import { AuthContext } from "../../context/AuthContext"
 import { PessoaDTO } from "../../model/PessoaDTO"
 import { ContainerList, DivTitle, TdNome, Table, Tr, Td, Th, TitleUsers, ContainerUsers } from "./Users.styles"
+import { ContainerForm,
+  BsPencilStyled,
+  ContainerAddressPage,
+  AiOutlineDeleteStyled, } from '../address/Address.styles'
+import { DivForm } from "../login/Login.styles";
 
 function Users() {
   const [users, setUsers] = useState<PessoaDTO['pessoa']>([])
   const [loading, setLoading] = useState(true)
+  const [atualizar, setAtualizar] = useState<boolean>(false);
+  const [idUpdate, setIdUpdate] = useState<number>();
 
   useEffect(()=>{
     getUsers();
@@ -24,12 +31,145 @@ function Users() {
   }
   console.log(users)
 
+  const formikProps = useFormik({
+    initialValues: {
+      nome: "",
+      logradouro: "",
+      bairro: "",
+      localidade: "",
+      uf: "",
+      pais: "",
+      tipo: "RESIDENCIAL",
+      complemento: "",
+      numero: "",
+    },
+    onSubmit: async (values: any, actions: any) => {
+      console.log(atualizar);
+      if (!atualizar) {
+        // await postAddress(values);
+      }
+      if (atualizar) {
+        // await updateAddress();
+      }
+      actions.setSubmitting(false);
+    },
+  });
+
   if(loading){
     return(<h1>Loading</h1>)
   }
 
   return (
     <ContainerUsers>
+      <ContainerUsers>
+        <DivTitle>
+          <TitleUsers>Cadastrar Usuário</TitleUsers>
+        </DivTitle>
+        <ContainerForm>
+          <form onSubmit={formikProps.handleSubmit}>
+            <DivForm>
+              <label htmlFor="nome">Nome:</label>
+              <input
+                id="nome"
+                name="nome"
+                placeholder="Digite seu nome"
+                value={formikProps.values.nome}
+                onChange={formikProps.handleChange}
+              />
+            </DivForm>
+
+            <DivForm>
+              <label htmlFor="logradouro">Logradouro:</label>
+              <input
+                id="logradouro"
+                name="logradouro"
+                placeholder="Digite seu logradouro"
+                value={formikProps.values.logradouro}
+                onChange={formikProps.handleChange}
+              />
+            </DivForm>
+
+            <DivForm>
+              <label htmlFor="numero">Número:</label>
+              <input
+                id="numero"
+                name="numero"
+                placeholder="Digite o número da sua residência"
+                value={formikProps.values.numero}
+                onChange={formikProps.handleChange}
+              />
+            </DivForm>
+
+            <DivForm>
+              <label htmlFor="complemento">Complemento:</label>
+              <input
+                id="complemento"
+                name="complemento"
+                placeholder="Digite o seu complemento"
+                value={formikProps.values.complemento}
+                onChange={formikProps.handleChange}
+              />
+            </DivForm>
+
+            <DivForm>
+              <label htmlFor="bairro">Bairro:</label>
+              <input
+                id="bairro"
+                name="bairro"
+                placeholder="Digite seu bairro"
+                value={formikProps.values.bairro}
+                onChange={formikProps.handleChange}
+              />
+            </DivForm>
+
+            <DivForm>
+              <label htmlFor="localidade">Cidade:</label>
+              <input
+                id="localidade"
+                name="localidade"
+                placeholder="Digite sua localidade"
+                value={formikProps.values.localidade}
+                onChange={formikProps.handleChange}
+              />
+            </DivForm>
+
+            <DivForm>
+              <label htmlFor="uf">Estado:</label>
+              <input
+                id="uf"
+                name="uf"
+                placeholder="Digite a sigla do seu estado"
+                value={formikProps.values.uf}
+                onChange={formikProps.handleChange}
+              />
+            </DivForm>
+
+            <DivForm>
+              <label htmlFor="pais">País:</label>
+              <input
+                id="pais"
+                name="pais"
+                placeholder="Digite o seu país"
+                value={formikProps.values.pais}
+                onChange={formikProps.handleChange}
+              />
+            </DivForm>
+
+            <DivForm>
+              <label htmlFor="tipo">Tipo de contato:</label>
+              <select
+                name="tipo"
+                value={formikProps.values.tipo}
+                onChange={formikProps.handleChange}
+              >
+                <option value="RESIDENCIAL">Residencial</option>
+                <option value="COMERCIAL">Comercial</option>
+              </select>
+            </DivForm>
+
+            <button type="submit">Submit</button>
+          </form>
+        </ContainerForm>
     <DivTitle>
       <TitleUsers>Usuários</TitleUsers>
     </DivTitle>
@@ -63,6 +203,7 @@ function Users() {
       </tbody>
       </Table> 
    </ContainerList>
+   </ContainerUsers>
    </ContainerUsers>
   )
 }
